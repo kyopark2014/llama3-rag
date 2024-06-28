bb35ef92007580b9322f9144f4ab46f079d2fb3a
Kyoungsu Park <kyopark2014@gmail.com>
2024-06-28 17:49:14 +0900

    update

---
+++ cdk-llama3-rag-bot/test/cdk-llama3-rag-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLlama3RagBot from '../lib/cdk-llama3-rag-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-llama3-rag-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLlama3RagBot.CdkLlama3RagBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
